33838a4bad44ca71fd54bec5a5dbd49c9d08f517
F1yboyWi11iams <sjwilliams3@students.unwsp.edu>
2024-09-09 18:30:13 -0500

    Update program_1.py
    Williams Edit

--- program_1.py
+++ program_1.py
@@ -1,9 +1,8 @@
 def personal_information():
-    print('Seth Williams')
-    print('50180 211th Street, Lake Crystal, MN 56055')
-    print('(shared number) 706-604-8807')
-    print('(college major) Undecided')
-
+    print('Name: Seth Williams
+          Adress: 50180 211th Street, Lake Crystal, MN 56055
+          Phone: 706-604-8807
+          College Major: Undecided')
 
 # Line which calls the above function.
 personal_information()
